ad0165ed4ac9c12b67bbd640ea998b235eb81d68
github-action-benchmark <github@users.noreply.github.com>
2021-04-19 02:54:20 +0000

    add sync_mpsc (cargo) benchmark result for d6da67b2e612455e16c11967c762ae802c2ac428

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618800839733,
+  "lastUpdate": 1618800860083,
   "repoUrl": "https://github.com/sunjay/tokio",
   "entries": {
     "sync_semaphore": [
@@ -1121,6 +1121,90 @@ window.BENCHMARK_DATA = {
             "name": "uncontented_unbounded",
             "value": 694477,
             "range": "± 6184",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "eliza@buoyant.io",
+            "name": "Eliza Weisman",
+            "username": "hawkw"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d6da67b2e612455e16c11967c762ae802c2ac428",
+          "message": "sync: add `mpsc::Sender::{reserve_owned, try_reserve_owned}` (#3704)\n\n* sync: add `mpsc::Sender::{reserve_owned, try_reserve_owned}`\r\n\r\n## Motivation\r\n\r\nThe `mpsc::Sender::reserve` method currently returns a permit that borrows\r\nfrom the `Sender`. It would be nice to have a version of it that returns\r\nan owned permit.\r\n\r\n## Solution\r\n\r\nThis branch adds an `OwnedPermit` type and `Sender::{reserve_owned,\r\ntry_reserve_owned}` methods. Unlike the comparable methods on\r\n`Semaphore`, these methods do *not* require an `Arc<Sender>` as the\r\nreceiver; this is because the sender internally reference counts the\r\nchannel and is already cheap to clone. Requiring an `Arc` would simply\r\nadd an unnecessary second layer of reference counting, which is not\r\nideal; instead, the documentation encourages the user to clone the\r\nsender prior to calling `reserve_owned` when necessary.\r\n\r\nSince these methods take the `Sender` by value, they also have the ability\r\nto _return_ the `Sender` from a successful `OwnedPermit::send`. This\r\nallows them to be used without additional clones. Essentially, this is a\r\nvery simple type-level encoding of the sender's state, with the\r\ntransitions\r\n```\r\n      ┌──────┐\r\n ┌───►│Sender├───┐\r\n │    └──────┘   │\r\nsend          reserve\r\n │ ┌───────────┐ │\r\n └─┤OwnedPermit│◄┘\r\n   └───────────┘\r\n```\r\n\r\nAdditionally, I added an `OwnedPermit::release`, which returns the\r\n`Sender` and releases the permit *without* sending a message.\r\n\r\nCloses #3688 \r\n\r\nSigned-off-by: Eliza Weisman <eliza@buoyant.io>",
+          "timestamp": "2021-04-15T12:55:57-07:00",
+          "tree_id": "bf29d9dc513a8d80cadd15021e58c979e5f6d691",
+          "url": "https://github.com/sunjay/tokio/commit/d6da67b2e612455e16c11967c762ae802c2ac428"
+        },
+        "date": 1618800857435,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "contention_bounded",
+            "value": 6914175,
+            "range": "± 2862413",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_bounded_full",
+            "value": 6414909,
+            "range": "± 1447294",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_unbounded",
+            "value": 6319213,
+            "range": "± 3122500",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_000_medium",
+            "value": 741,
+            "range": "± 136",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_medium",
+            "value": 786,
+            "range": "± 242",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_1_medium",
+            "value": 744,
+            "range": "± 107",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_large",
+            "value": 135447,
+            "range": "± 18407",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_medium",
+            "value": 1622,
+            "range": "± 246",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_bounded",
+            "value": 986639,
+            "range": "± 173839",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_unbounded",
+            "value": 724628,
+            "range": "± 162020",
             "unit": "ns/iter"
           }
         ]
