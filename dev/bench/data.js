bdc0a2406e5c460cd6774facb79d7711e9ed38fc
github-action-benchmark <github@users.noreply.github.com>
2021-04-21 23:54:26 +0000

    add sync_semaphore (cargo) benchmark result for 2b9b55810847b4c7855e3de82f432ca997600f30

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1619049258772,
+  "lastUpdate": 1619049266598,
   "repoUrl": "https://github.com/sunjay/tokio",
   "entries": {
     "sync_semaphore": [
@@ -269,6 +269,60 @@ window.BENCHMARK_DATA = {
             "name": "uncontended_concurrent_single",
             "value": 1052,
             "range": "± 127",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@carllerche.com",
+            "name": "Carl Lerche",
+            "username": "carllerche"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2b9b55810847b4c7855e3de82f432ca997600f30",
+          "message": "time: prevent `time::advance` from going too far (#3712)\n\nPreviously, `time::advance` would set the mocked clock forward the\r\nrequested amount, then yield. However, if there was no work ready to\r\nperform immediately, this would result in advancing to the next expiring\r\nsleep.\r\n\r\nNow, `time::advance(...)` will unblock at the requested time. The\r\ndifference between `time::advance(...)` and `time::sleep(...)` is a bit\r\nfuzzy. The main difference is `time::sleep(...)` operates on the current\r\ntask and `time::advance(...)` operates at the runtime level.\r\n\r\nFixes #3710",
+          "timestamp": "2021-04-21T15:23:35-07:00",
+          "tree_id": "6e5d0c6d86afdeaee3700f2851df5e78bfe7f8a4",
+          "url": "https://github.com/sunjay/tokio/commit/2b9b55810847b4c7855e3de82f432ca997600f30"
+        },
+        "date": 1619049265200,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "contended_concurrent_multi",
+            "value": 14576,
+            "range": "± 5474",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contended_concurrent_single",
+            "value": 1101,
+            "range": "± 178",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontended",
+            "value": 622,
+            "range": "± 64",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontended_concurrent_multi",
+            "value": 14851,
+            "range": "± 5971",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontended_concurrent_single",
+            "value": 1110,
+            "range": "± 137",
             "unit": "ns/iter"
           }
         ]
